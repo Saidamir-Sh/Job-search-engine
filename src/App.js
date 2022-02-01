ebbc600181caa316bdfc0e562d6d3a96b3fef630
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-02-01 22:39:21 +0100

    component templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Filter from './components/Filter';
+import JobList from './components/JobList';
+import { Container, Col, Row } from "react-bootstrap"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <Container>
+      <Row>
+        <Col md={12}>
+          <Navbar />
+        </Col>
+        <Col md={4}>
+          <Filter />
+        </Col>
+        <Col md={7}>
+          <JobList />
+        </Col>
+      </Row>
+    </Container>
+    </BrowserRouter>
   );
 }
 
