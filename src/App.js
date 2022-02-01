7ca05f29f3d1482c473ccdcc2f406434190dab97
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-02-01 23:47:03 +0100

    component routing

--- src/App.js
+++ src/App.js
@@ -3,27 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Filter from './components/Filter';
 import JobList from './components/JobList';
+import SavedJobs from './components/SavedJobs';
 import { Container, Col, Row } from "react-bootstrap"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import JobDetail from './components/JobDetail';
 
 
 function App() {
   return (
-    <BrowserRouter>
     <Container>
-      <Row>
-        <Col md={12}>
-          <Navbar />
-        </Col>
-        <Col md={4}>
-          <Filter />
-        </Col>
-        <Col md={7}>
-          <JobList />
-        </Col>
-      </Row>
+      <BrowserRouter>
+        <Row>
+          <Col md={12}>
+            <Routes>
+                <Route exact path='/' element={<Navbar />} />
+            </Routes>
+          </Col>
+          <Col md={4}>
+            <Routes>
+              <Route path='/' element={<Filter />}/>
+              </Routes>
+          </Col>
+          <Col md={7}>
+            <Routes>
+              <Route path='/' element={<JobList />}/>
+              </Routes>
+          </Col>
+        </Row>
+        <Routes>
+            <Route path='/company/:company_name' element={<JobDetail/>} />
+            <Route path='/savedjobs' element={<SavedJobs />}/>
+        </Routes>
+      </BrowserRouter>
     </Container>
-    </BrowserRouter>
   );
 }
 
--- src/components/JobDetail.jsx
+++ src/components/JobDetail.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const JobDetail = () => {
   return (
-    <div></div>
+    <div>
+      jobdetail
+    </div>
   );
 }
 
